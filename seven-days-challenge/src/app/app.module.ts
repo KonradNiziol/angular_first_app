4e8a02a01d8e468b0a0ae24c6b55f64ba3424c17
Konrad Niziol <niziol.konrad89@gmail.com>
2020-05-10 12:38:34 +0200

    Add empty mat-toolbar

--- seven-days-challenge/src/app/app.module.ts
+++ seven-days-challenge/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarComponent } from './mat-toolbar/mat-toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatToolbarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
